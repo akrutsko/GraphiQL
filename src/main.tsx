a76580d7469071d35ecaa2f77e600412dd6a082d
Aliaksei Krutsko <wonder4ik@gmail.com>
2023-12-06 22:44:06 +0300

    chore: create project folder structure

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import './main.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import App from './app/App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
