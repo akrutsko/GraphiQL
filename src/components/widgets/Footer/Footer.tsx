aed08de206873d4ff0dfa1845e877f706d18fc26
stroich <stroichmwork@gmail.com>
2024-01-04 20:53:29 +0400

    fix: fix footer

--- src/components/widgets/Footer/Footer.tsx
+++ src/components/widgets/Footer/Footer.tsx
@@ -1,20 +1,28 @@
-import { useTranslation } from '../../../hooks';
+import Tooltip from '@mui/material/Tooltip';
+
+import { useTheme, useTranslation } from '../../../hooks';
 
 import styles from './Footer.module.css';
 
 const Footer = () => {
   const translation = useTranslation();
+  const theme = useTheme();
+
+  const stylesIcon = {
+    filter: theme === 'dark' ? 'invert(100%)' : 'none',
+  };
+
   return (
     <footer data-testid="footer" className={styles.footer}>
       <span> &copy; 2023 - GraphiQL</span>
       <div className={styles.devContainer}>
         {translation.developers.map(({ name, href }) => (
-          <a key={name} href={href} target="_blank" rel="noreferrer">
-            {name}
-          </a>
+          <Tooltip title={name} key={name}>
+            <a className={styles.githubLogo} href={href} target="_blank" rel="noreferrer" style={stylesIcon}></a>
+          </Tooltip>
         ))}
       </div>
-      <a className={styles.rsLogo} href="https://rs.school/react/" target="_blank" rel="noreferrer" />
+      <a className={styles.rsLogo} href="https://rs.school/react/" target="_blank" rel="noreferrer" style={stylesIcon} />
     </footer>
   );
 };
